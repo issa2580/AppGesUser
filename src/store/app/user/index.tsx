470ae59fe661a983656bb946d8d31944740e9c56
issa2580 <issadiop2580@gmail.com>
2023-03-27 11:50:08 +0000

    add type and interface

---
+++ src/store/app/user/index.tsx
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { Dispatch } from "@reduxjs/toolkit";
+import axiosConfig from "../../../utils/config";
+
+interface userType {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface Redux {
+    getState: any;
+    dispatch: Dispatch<any>;
+}
+
+interface postResponseType {
+    data: userType;
+    message: string;
+}
+
